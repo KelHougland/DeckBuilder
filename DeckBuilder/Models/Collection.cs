10a8f32cea81e2da13fe9ca5b03bc60230ec8829
Kelly Hougland <knhougland@gmail.com>
2018-01-06 10:27:23 -0600

    did initial set-up of models for databases

---
+++ DeckBuilder/Models/Collection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeckBuilder.Models
+{
+    public class Collection
+    {
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public IList<CardCollection> CardCollections { get; set; }
+    }
+}
